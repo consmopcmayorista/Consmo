1251b6ce152f456039595321261919182a2fbc9f
consmopcmayorista <consmopcmayorista2017@gmail.com>
2025-02-08 11:39:05 -0500

    htaccess

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { copyFileSync } from 'fs'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -20,10 +22,24 @@ export default defineConfig({
   build: {
     commonjsOptions: {
       include: [/@vue-leaflet\/vue-leaflet/, /node_modules/]
-    }
+    },
+    emptyOutDir: false, // No borra la carpeta dist en cada build
+    outDir: 'dist', // Carpeta de salida
   },
   base: '/',
   server: {
-    historyApiFallback: true, // Permite que Vite maneje rutas SPA
-  },
+    historyApiFallback: true, // Permite que Vite maneje rutas SPA
+  },
+  // Hook post-build para copiar el .htaccess
+  async closeBundle() {
+    try {
+      copyFileSync(
+        path.resolve('public/.htaccess'),
+        path.resolve('dist/.htaccess')
+      )
+      console.log('✅ .htaccess copiado a dist/')
+    } catch (error) {
+      console.error('⚠️ Error copiando .htaccess:', error)
+    }
+  }
 })
