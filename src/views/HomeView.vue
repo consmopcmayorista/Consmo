3f329b40e8ffd0fe5d2d853260f415b7055c0af8
consmopcmayorista <consmopcmayorista2017@gmail.com>
2025-03-01 10:00:13 -0500

    imagenes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -442,7 +442,7 @@ $(document).ready(function() {
   <services />
   
   <Destacados />
-
+<!--
   <div class="ad_banner_area section_padding_b">
     <div class="container">
       <a href="/promociones">
@@ -460,7 +460,7 @@ $(document).ready(function() {
         </picture>
       </a>
     </div>
-  </div>
+  </div>-->
 
   <!-- Sección Categorías -->
    <!--
