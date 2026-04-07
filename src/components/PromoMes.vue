420209bfcfe53a423a81440f4fb46ea2bbd6364f
consmopcmayorista <consmopcmayorista2017@gmail.com>
2026-04-07 10:51:53 -0500

    Se montan imagenes

--- src/components/PromoMes.vue
+++ src/components/PromoMes.vue
@@ -15,31 +15,15 @@
   </div>
 </template>
   
-    <script setup> 
-  const promos = [
-    { title: 'IMPRESORA MULTIFUNCIONAL WIFI EPSON L3251: 5589', image: '5589.jpg'},
-    { title: 'IMPRESORA MULTIFUNCIONAL USB EPSON L3210: 2416', image: '2416.jpg'},
-    { title: 'IMPRESORA MULTIFUNCIONAL WIFI CANON PIXMA G3170: 2412', image: '2412.jpg'},
-    { title: 'COMBO POS GRANDE CAJON + IMPRESORA 80MM + LECTOR: 3392-3981-3635', image: '3392-3981-3635.jpg'},
-    { title: 'COMBO POS PEQUEÑO CAJON + IMPRESORA 58MM + LECTOR: 3393-3635-3619', image: '3393-3635-3619.jpg'},
-    { title: 'LECTOR CODIGO DE BARRAS OMNIDIRECCIONAL 2D ALAMBRICO: 3685', image: '3685.jpg'},
-    { title: 'IMPRESORA TERMICA POS 80MM USB+LAN+RJ11 NEOTEK : 3981', image: '3981.jpg'},
-    { title: 'ROUTER NETIS INALAMBRICO N300 3 ANTENAS 5DBI: NET4004', image: 'NET4004.jpg'},
-    { title: 'EQUIPO COMPLETO USADO TINY I5 6TA 8GB RAM/256GB SSD + MONITOR 19: U2152', image: 'U2152.jpg'},
-    { title: 'TECLADO ALAMBRICO RGB COLOR NEGRO JEDEL K500+: 2236', image: '2236.jpg'},
-    { title: 'IMPRESORA TERMICA POS 80MM USB+LAN+BLUETOOTH DIGITAL POS: 2409', image: '2409.jpg'},
-    { title: 'CAJON MONEDERO PEQUEÑO 4 BILLETES 5 MONEDAS METALICO DIGITAL POS: 3694', image: '3694.jpg'},
-    { title: 'CAMARA IP WIFI6 ROBOTICA AI 5MP TENDA MOD 3K CP7: 1696', image: '1696.jpg'},
-    { title: 'ROUTER DUAL-BAND 5 ANTENAS 6DBI GIGABIT WIFI6 TENDA TX2JPRO: 2693', image: '2693.jpg'},
-    { title: 'CAJON MONEDERO GRANDE 5 BILLETES 8 MONEDAS NEOTEK: 3392', image: '3392.jpg'},
-
-
+   <script setup> 
+  const promos = [[
+   //{ title: 'IMPRESORA MULTIFUNCIONAL WIFI EPSON L3251: 5589', image: '5589.jpg'},
 
     
     
   
      // Agrega más promociones según sea necesario
-  ]
+  ]]
   </script>
   
   <style scoped>
